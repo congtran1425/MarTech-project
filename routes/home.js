c83c18301c5555b09d6d108549451e38aeefd12a
chicong1425 <chicongcec14@gmail.com>
2026-04-02 23:56:48 +0700

    Show DB error code on home warning

--- routes/home.js
+++ routes/home.js
@@ -22,7 +22,11 @@ module.exports = (app) => {
       });
     } catch (err) {
       console.error(err);
-      warning = '\u004b\u0068\u00f4\u006e\u0067\u0020\u0074\u1ea3\u0069\u0020\u0111\u01b0\u1ee3\u0063\u0020\u0064\u1eef\u0020\u006c\u0069\u1ec7\u0075';
+      const errCode =
+        (err && (err.code || err.errno)) ? String(err.code || err.errno) : 'UNKNOWN';
+      warning = '\u004b\u0068\u00f4\u006e\u0067\u0020\u0074\u1ea3\u0069\u0020\u0111\u01b0\u1ee3\u0063\u0020\u0064\u1eef\u0020\u006c\u0069\u1ec7\u0075\u0020\u0028\u0044\u0042\u003a\u0020' +
+        errCode +
+        '\u0029';
       return res.status(200).render('home/index', {
         title: '\u0054\u0072\u0061\u006e\u0067\u0020\u0063\u0068\u1ee7',
         categories: [],
