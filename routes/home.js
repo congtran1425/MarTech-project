bc83a77de238d94efce15c194d25b97ba0577437
chicong1425 <chicongcec14@gmail.com>
2026-04-02 23:52:36 +0700

    Fix home data loading on Vercel

--- routes/home.js
+++ routes/home.js
@@ -1,25 +1,38 @@
 module.exports = (app) => {
-  app.get('/', (req, res) => {
-    let success; let warning = app.helpers.msg(req);
-    let categories; let products;
+  app.get('/', async (req, res) => {
+    let success;
+    let warning = app.helpers.msg(req);
     const connection = app.dao.connectionFactory();
     const categoriesDAO = new app.dao.categoriesDAO(connection);
     const productsDAO = new app.dao.productsDAO(connection);
 
-    categoriesDAO.list()
-        .then((result) => categories = result)
-        .catch((err) => warning = 'Không thể tải danh mục');
-    productsDAO.list(9)
-        .then((result) => products = result)
-        .catch((err) => warning = 'Không thể tải sản phẩm');
+    try {
+      const [categories, products] = await Promise.all([
+        categoriesDAO.list(),
+        productsDAO.list(9),
+      ]);
 
-    setTimeout(() => {
-      res.status(200).render('home/index', {
-        title: 'Trang chủ',
-        categories, products,
-        success, warning,
+      return res.status(200).render('home/index', {
+        title: '\u0054\u0072\u0061\u006e\u0067\u0020\u0063\u0068\u1ee7',
+        categories,
+        products,
+        success,
+        warning,
         user: req.session['user'],
       });
-    }, 100);
+    } catch (err) {
+      console.error(err);
+      warning = '\u004b\u0068\u00f4\u006e\u0067\u0020\u0074\u1ea3\u0069\u0020\u0111\u01b0\u1ee3\u0063\u0020\u0064\u1eef\u0020\u006c\u0069\u1ec7\u0075';
+      return res.status(200).render('home/index', {
+        title: '\u0054\u0072\u0061\u006e\u0067\u0020\u0063\u0068\u1ee7',
+        categories: [],
+        products: [],
+        success,
+        warning,
+        user: req.session['user'],
+      });
+    } finally {
+      connection.end();
+    }
   });
 };
